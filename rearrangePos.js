30d92e563afb11ee31f0e2aa22d0f80abe6373d9
CrashStark <starkcrash0@gmail.com>
2023-11-13 23:55:10 +0530

    selection sort

---
+++ rearrangePos.js
@@ -0,0 +1,12 @@
+let arr=[1,2,-4,-5];
+let neRray=[];
+for (let i=0;i<arr.length;i++){
+    for(let j=0;j<arr.length;j++){
+       if(arr[i]>0 && arr[i]<a[j]){
+        neRray.push(arr[i])
+       }else if(arr[i]<0)
+       {
+        
+       }
+    }
+}
---
+++ selectionSort.js
@@ -0,0 +1,11 @@
+let arr=[7, 5, 9, 2, 8];
+let num=0;
+for(let i=0;i<arr.length;i++)
+{
+    for(let j=i+1;j<arr.length;j++){
+        if(arr[j]>arr[j+1])
+        {
+            
+        }
+    }
+}
