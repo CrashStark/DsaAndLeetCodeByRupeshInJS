ca76f1fb95a249b785b917058cb4a9784af57811
CrashStark <starkcrash0@gmail.com>
2023-12-15 20:32:23 +0530

    change to print pattern

--- pattern.js
+++ pattern.js
@@ -1,7 +1,15 @@
-for(let i=0;i<5;i++){
-    for(let j=0;j<5;j++){
-        process.stdout.write("*");
-        
-    }
-    console.log();
+for (let i = 1; i < 10; i++) {
+  for (let j = 10; j >= i; j--) {
+    process.stdout.write(" ");
+  }
+  for (let j = 1; j <= i; j++) {
+    process.stdout.write("* ");
+  }
+  for (let j = 10; j >= i; j--) {
+    process.stdout.write(" *");
+  }
+  for (let j = 1; j <= i; j++) {
+    process.stdout.write(" ");
+  }
+  console.log();
 }
