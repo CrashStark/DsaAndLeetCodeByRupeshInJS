c3cee6efc35c26ad1bdf9d99711d32e91a2ae707
CrashStark <starkcrash0@gmail.com>
2023-12-15 15:18:35 +0530

    Add LinkedList

---
+++ linkLists.js
@@ -0,0 +1,27 @@
+class List {
+  constructor(VALUE) {
+    this.head = {
+      value: VALUE,
+      next: null,
+    };
+
+      this.tail = this.head;
+      console.log(this.tail);
+
+  }
+  appendNode(node) {
+    let newNod ={
+        value:node,
+        next:null
+    }
+    this.head.next=newNod;
+    this.tail=newNod;
+  }
+}
+
+var ls=new List(10);
+
+ls.appendNode(20);
+console.log(ls);
+ls.appendNode(30);
+console.log(ls);
