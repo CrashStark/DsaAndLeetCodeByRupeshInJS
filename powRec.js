83e12cbbcf221341766882650bfb6ce7b41e8621
unknown <starkcrash0@gmail.com>
2023-12-19 04:46:58 -0800

    Power using recursion1

--- powRec.js
+++ powRec.js
@@ -1,11 +1,13 @@
 let power = 10;
 let num = 2;
 let n = 1
-function powFunction(num) {
-    if (n == power) {
-        return pow;
+function powFunction(num, pow) {
+    if (pow == 0) {
+        return 1;
     }
-    
-    return 2*powFunction(2);
+
+    return 2 * powFunction(2, power - 1);
 
 }
+
+console.log(powFunction(2,power));
